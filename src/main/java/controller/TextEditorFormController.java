6814cd652f72cde83c2e316b38c6ca60231ec629
theek23 <theekshanadesilva28@gmail.com>
2023-08-19 11:18:24 +0530

    button actions

--- src/main/java/controller/TextEditorFormController.java
+++ src/main/java/controller/TextEditorFormController.java
@@ -5,6 +5,7 @@ import com.jfoenix.controls.JFXTextArea;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.text.*;
+import javafx.scene.web.HTMLEditor;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,9 +14,34 @@ public class TextEditorFormController implements Initializable {
     public JFXTextArea txtArea;
     public JFXButton boldBtn;
     public TextFlow textFlow;
+    public HTMLEditor htmlEditor;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    public void newBtnOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void openBtnOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void openRecentOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void saveBtnOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void saveAsBtnOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void importBtnOnAction(ActionEvent actionEvent) {
 
     }
 }
